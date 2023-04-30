6b438adf69c453d2ce64bcc0ae5b2f70711913ed
Furkan Bulut <furkanbtng@gmail.com>
2023-04-30 14:05:22 +0300

    Admin panel designed

--- catalog/movies/admin.py
+++ catalog/movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Movie
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id','name','created_date','isPublished')
+    list_display_links = ('id','name')
+    list_filter = ('created_date','id')
+    list_editable = ('isPublished',)
+    search_fields = ('name','description')
+    list_per_page = 20
+admin.site.register(Movie,MovieAdmin)
--- catalog/movies/models.py
+++ catalog/movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100,verbose_name='Movie Name')
+    description = models.TextField(verbose_name='Film description')
+    image = models.CharField(max_length=50,verbose_name='Film image')
+    created_date= models.DateTimeField(auto_now_add=True,verbose_name='Film create date')
+    isPublished = models.BooleanField(default=True)
+
+    def __str__(self):
+        return  self.name
